e39a44a35b747789936d016af168970a732ea4c1
StrattaCloud <jeffsupwork1@gmail.com>
2024-03-17 17:51:59 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,6 +3,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/health')
+def health_check():
+    return 'OK', 200
+
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
